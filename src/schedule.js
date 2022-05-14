4e2e3ef731e40f9681a5588a126d2ca7bfd77a55
codetann <thetannerthomas@gmail.com>
2022-05-13 22:24:45 -0600

    add extra check

--- src/schedule.js
+++ src/schedule.js
@@ -2,7 +2,7 @@ import schedule from "node-schedule";
 
 function checkStores(callback) {
   const rule = new schedule.RecurrenceRule();
-  rule.hour = [9, 17];
+  rule.hour = [9, 14, 17];
 
   schedule.scheduleJob(rule, callback);
 }
